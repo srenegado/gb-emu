41ad74509834dc951ed3369ebfeb93b78b799344
srenegado <scottstationary@gmail.com>
2024-12-03 10:31:04 -0800

    Added simple window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <SDL.h>
 
 int main() {
-    std::cout << "GameBoy emulator!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("GB", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160 * 4, 144 * 4, 0);
+
+    // Main loop
+    int running = 1;
+    while (running) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case (SDL_KEYDOWN):
+                    if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) 
+                        running = 0;
+                    break;
+            }
+        }
+    }
+
     return 0;
 }
