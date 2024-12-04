9c3f02cc5528182435512fd23581f0a2f01339c8
srenegado <scottstationary@gmail.com>
2024-12-04 12:36:08 -0800

    Add simple CPU with memory and Register classes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,91 @@
 #include <iostream>
-#include <SDL.h>
+#include <cstdint>
+#include "SDL.h"
+
+
+
+class Register {
+    private:
+        uint8_t hi;
+        uint8_t lo;
+
+    public:
+        // Constructor
+        Register() {
+            hi = 0x0000;
+            lo = 0x0000;
+        }
+
+        // Destructor
+        ~Register() {
+            
+        }
+
+        void set_hi(uint8_t val) {
+            hi = val;
+        }
+
+        void set_lo(uint8_t val) {
+            lo = val;
+        }
+
+};
+
+
+class CPU {
+    private:
+
+        // Registers
+        Register AF;    // Accumulator and flags
+        Register BC;    // General registers
+        Register DE;
+        Register HL;
+
+        uint16_t PC;    // Program counter
+        uint16_t SP;    // Stack pointer        
+
+
+        // Memory
+        uint8_t mem[0x10000];
+
+    public:
+    
+        // Constructor
+        CPU(): AF(), BC(), DE(), HL()  
+        {
+            PC = 0x0000;
+            SP = 0x0000;
+        }
+
+        // Destructor
+        ~CPU() {
+
+        }
+
+    
+        void emulate_cycle() {
+            // Fetch
+            uint8_t opcode = mem[PC];
+
+            // Decode
+
+            // Execute
+        
+        }
+
+};
+
+
+
+
+
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("GB", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160 * 4, 144 * 4, 0);
+    SDL_Window *window = SDL_CreateWindow("gb-emu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160 * 4, 144 * 4, 0);
 
+    CPU cpu;
+    
     // Main loop
     int running = 1;
     while (running) {
@@ -21,4 +102,4 @@ int main() {
     }
 
     return 0;
-}
+}   
