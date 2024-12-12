c7bc7d2affa15ec920e0428a30ebc27a45a7aadb
srenegado <scottstationary@gmail.com>
2024-12-12 14:38:59 -0800

    Reset Tile class

--- graphics.h
+++ graphics.h
@@ -7,17 +7,10 @@
  */
 class Tile {
     private:
-        uint8_t pixels[8][8]; 
-        uint16_t addr;        // Address in memory
 
     public:
         Tile();
         ~Tile();
-
-        /** 
-         * Store tile into memory starting from its address
-         */
-        void store_tile(Memory &mem);
 };
 
 class PPU {
