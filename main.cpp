d09d9ca8bb96113ca62075722f0148d3381de523
srenegado <scottstationary@gmail.com>
2025-01-10 15:06:00 -0800

    Adjust timer ticking

--- main.cpp
+++ main.cpp
@@ -21,10 +21,12 @@ int main(int argc, char** argv) {
         return -1;
     }
     
-    // Main loop
+    // Main emulation loop
     int running = 1;
     unsigned int m_cycles = 0;
     while (running) {
+
+        // Quit if user pressed ESC
         SDL_Event event;
 
         while (SDL_PollEvent(&event)) {
@@ -36,11 +38,21 @@ int main(int argc, char** argv) {
             }
         }
 
+        // Don't overtick the system counter 
+        int carry_over = 0;
+        if (m_cycles > 0) {
+            carry_over = m_cycles;
+            m_cycles = 0;
+        }
+    
         // m_cycles += cpu.emulate_cycles()
 
         // System counter is incremented every M-cycle
         for (int i = 0; i < m_cycles; i++ ) 
-            cpu.inc_DIV(mem); 
+            cpu.inc_DIV(mem);
+
+        // Need running M-cycle count for incrementing timer accurately
+        m_cycles += carry_over; 
 
         // CPU timer is incremented every timer_clock_speed M-cycles
         if (cpu.is_timer_started(mem)) {
@@ -48,6 +60,8 @@ int main(int argc, char** argv) {
             int timer_ticks = m_cycles / timer_clock_speed;
             for (int i = 0; i < timer_ticks; i++)
                 cpu.inc_TIMA(mem);
+
+            m_cycles %= timer_clock_speed;
         }
     
     }
